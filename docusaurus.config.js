5666dbfdac80d7eecf30fe03ff35cb542e825eea
m6urns <m6urns@gmail.com>
2022-07-22 10:51:18 -0700

    theme?

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+const config = {
    title: 'Knowledge Wiki',
    url: 'https://m6urns.github.io',
    baseUrl: '/',
    themes: ['@docusaurus/theme-classic']
 };
+
+module.exports = config;
